ffd5043176e13383df89a50b9aa82df404f24476
Hanno Schreiber <hanno.schreiber@gmail.com>
2020-06-21 14:53:33 +0200

    add some files we forgot

---
+++ src/main/java/de/uni/haecksOnTheBeach2020/HaecksOnTheBeach2020Application.java
@@ -0,0 +1,13 @@
+package de.uni.haecksOnTheBeach2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaecksOnTheBeach2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaecksOnTheBeach2020Application.class, args);
+	}
+
+}
